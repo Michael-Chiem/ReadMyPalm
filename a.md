e59b129d2e3176f6c290928838d6e5dc625af401
Michael Chiem <michaelchiem1@hotmail.com>
2024-02-24 17:07:45 -0700

    Adding Image URL and fixed the readme.js codes

---
+++ a.md
@@ -0,0 +1,36 @@
+
+# a
+
+![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)
+![Project Image](https://i.ytimg.com/vi/jeOfS90Flf8/maxresdefault.jpg)
+
+## Description
+a
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+a
+
+## Usage
+a
+
+## License
+This project is licensed under the MIT license.
+
+## Contributing
+
+
+## Tests
+a
+
+## Questions
+For any questions regarding the project, contact .
+
+[Project Link]()
--- readme.js
+++ readme.js
@@ -4,11 +4,57 @@ const fs = require('fs');
 inquirer.prompt(
 
 [
-{type:'input',name:'title',message:'What is your project title?'},
-{type:'input',name:'description',message:'What is your project description?'},
-{type:'input',name:'installation',message:'how to install?'},
-{type:'input',name:'usage',message:'how to use?'},
-{type:'list', name:'license',message:'What is your project license?', choices: ['MIT', 'Apache', 'GPL', 'BSD', 'None']},
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Enter the project title:'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Enter a brief description of your project:'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Enter installation instructions:'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Enter usage instructions:'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for your project:',
+        choices: ['MIT', 'Apache', 'GPL', 'BSD', 'None']
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Enter guidelines for contributing to your project:'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Enter instructions for running tests:'
+    },
+    {
+        type: 'input',
+        name: 'questions',
+        message: 'Enter your contact information for questions:'
+    },
+    {
+        type: 'input',
+        name: 'imageUrl',
+        message: 'Enter the URL of an image to attach:'
+    },
+    {
+        type: 'input',
+        name: 'projectLink',
+        message: 'Enter the project link:'
+    }
 
 
 //WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
@@ -28,20 +74,37 @@ function createReadMe(answers) {
 # ${answers.title}
 
 ![GitHub license](https://img.shields.io/badge/license-${answers.license}-blue.svg)
-## Table of Contents 
-
-* [Description](#description)
-* [Installation](#installation)
-* [Usage](#usage)
-
+![Project Image](${answers.imageUrl})
 
 ## Description
 ${answers.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
 ## Installation
 ${answers.installation}
+
 ## Usage
 ${answers.usage}
+
 ## License
-${answers.license}
-    `
+This project is licensed under the ${answers.license} license.
+
+## Contributing
+${answers.contributing}
+
+## Tests
+${answers.tests}
+
+## Questions
+For any questions regarding the project, contact ${answers.questions}.
+
+[Project Link](${answers.projectLink})
+`;
 }
