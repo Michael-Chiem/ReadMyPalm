e1e905e46955c73a45dfe146b66360dfd1e99872
Michael Chiem <michaelchiem1@hotmail.com>
2024-02-24 17:26:34 -0700

    Updating the Readme.js file by adding confirm function

--- readme.js
+++ readme.js
@@ -1,76 +1,83 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-inquirer.prompt(
+function promptCreateReadme() {
+    return inquirer.prompt([
+        {
+            type: 'confirm',
+            name: 'createReadme',
+            message: 'Do you want to create a README file?',
+            default: true
+        }
+    ]);
+}
 
-[
-    {
-        type: 'input',
-        name: 'title',
-        message: 'Enter the project title:'
-    },
-    {
-        type: 'input',
-        name: 'description',
-        message: 'Enter a brief description of your project:'
-    },
-    {
-        type: 'input',
-        name: 'installation',
-        message: 'Enter installation instructions:'
-    },
-    {
-        type: 'input',
-        name: 'usage',
-        message: 'Enter usage instructions:'
-    },
-    {
-        type: 'list',
-        name: 'license',
-        message: 'Choose a license for your project:',
-        choices: ['MIT', 'Apache', 'GPL', 'BSD', 'None']
-    },
-    {
-        type: 'input',
-        name: 'contributing',
-        message: 'Enter guidelines for contributing to your project:'
-    },
-    {
-        type: 'input',
-        name: 'tests',
-        message: 'Enter instructions for running tests:'
-    },
-    {
-        type: 'input',
-        name: 'questions',
-        message: 'Enter your contact information for questions:'
-    },
-    {
-        type: 'input',
-        name: 'imageUrl',
-        message: 'Enter the URL of an image to attach:'
-    },
-    {
-        type: 'input',
-        name: 'projectLink',
-        message: 'Enter the project link:'
+promptCreateReadme().then((response) => {
+    if (response.createReadme) {
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'title',
+                message: 'Enter the project title:'
+            },
+            {
+                type: 'input',
+                name: 'description',
+                message: 'Enter a brief description of your project:'
+            },
+            {
+                type: 'input',
+                name: 'installation',
+                message: 'Enter installation instructions:'
+            },
+            {
+                type: 'input',
+                name: 'usage',
+                message: 'Enter usage instructions:'
+            },
+            {
+                type: 'list',
+                name: 'license',
+                message: 'Choose a license for your project:',
+                choices: ['MIT', 'Apache', 'GPL', 'BSD', 'None']
+            },
+            {
+                type: 'input',
+                name: 'contributing',
+                message: 'Enter guidelines for contributing to your project:'
+            },
+            {
+                type: 'input',
+                name: 'tests',
+                message: 'Enter instructions for running tests:'
+            },
+            {
+                type: 'input',
+                name: 'questions',
+                message: 'Enter your contact information for questions:'
+            },
+            {
+                type: 'input',
+                name: 'imageUrl',
+                message: 'Enter the URL of an image to attach:'
+            },
+            {
+                type: 'input',
+                name: 'projectLink',
+                message: 'Enter the project link:'
+            }
+        ]).then((answers) => {
+            console.log(answers);
+            let data = createReadMe(answers);
+            fs.writeFileSync(`${answers.title}.md`, data);
+        });
+    } else {
+        console.log('No README file will be created. Exiting...');
     }
-
-
-//WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
-]
-
-
-).then((answers) => {console.log(answers);
-
-let data = createReadMe(answers);
-fs.writeFileSync(`${answers.title}.md`, data)}
-
-)
-
+});
 
 function createReadMe(answers) {
-    return`
+    return `
 # ${answers.title}
 
 ![GitHub license](https://img.shields.io/badge/license-${answers.license}-blue.svg)
